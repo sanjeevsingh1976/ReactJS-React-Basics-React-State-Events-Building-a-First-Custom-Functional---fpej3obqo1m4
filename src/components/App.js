4995fddb1f3bae222ca64daf4d4cbd08a23a4d64
sanjeev singh <101657713+sanjeevsingh1976@users.noreply.github.com>
2022-07-16 02:47:35 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React, { Component, useState } from "react";
-import '../styles/App.css';
+import Heading from "./Heading.js";
+import InputQuery from "./InputQuery.js";
+import SubHeading from "./SubHeading";
+import SubmitButton from "./SubmitButton";
+import "../styles/App.css";
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <InputQuery />
+      <SubHeading />
+      <SubmitButton />
     </div>
-  )
-}
+  );
+};
 
-
+export default App;
